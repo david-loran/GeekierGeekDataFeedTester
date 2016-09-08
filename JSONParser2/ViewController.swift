1b48e6381dc20e230dca26eeeda94b21ac3c853f
David Mitchell <david.carbon13@gmail.com>
2016-09-07 17:29:49 -0700

    Initial Commit

---
+++ JSONParser2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParser2
+//
+//  Created by David Mitchell on 9/7/16.
+//  Copyright © 2016 David Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
